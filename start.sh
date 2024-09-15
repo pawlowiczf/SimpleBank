5795a97bc98001c2c3c8564179c4737f05e5d172
Filip Pawlowicz <pawlowicz.filip137@gmail.com>
2024-09-15 19:14:13 +0200

    changed start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ set -e
 echo "run db migration"
 source /app/app.env
 echo "$DB_SOURCE"
+echo "$SERVER_ADDRESS"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up 
 
 echo "start the app"
