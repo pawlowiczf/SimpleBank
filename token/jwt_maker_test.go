9dff228276276df797609e062644c2c2a829deba
pawlowiczf <pawlowicz.filip137@gmail.com>
2024-10-02 14:11:12 +0200

    sessions, refresh_token

--- token/jwt_maker_test.go
+++ token/jwt_maker_test.go
@@ -18,11 +18,12 @@ func TestJWTMaker(t *testing.T) {
 	issuedAt := time.Now() 
 	expiredAt := issuedAt.Add(duration) 
 
-	token, err := maker.CreateToken(username, duration)
+	token, payload, err := maker.CreateToken(username, duration)
 	require.NoError(t, err)
+	require.NotEmpty(t, payload)
 	require.NotEmpty(t, token)
 
-	payload, err := maker.VerifyToken(token)
+	payload, err = maker.VerifyToken(token)
 	require.NoError(t, err)
 	require.NotEmpty(t, payload)
 
@@ -39,11 +40,12 @@ func TestExpiredJWTToken(t *testing.T) {
 	username := util.RandomOwner()
 	duration := -time.Minute
 
-	token, err := maker.CreateToken(username, duration)
+	token, payload, err := maker.CreateToken(username, duration)
 	require.NoError(t, err)
 	require.NotEmpty(t, token) 
+	require.NotEmpty(t, payload)
 
-	payload, err := maker.VerifyToken(token)
+	payload, err = maker.VerifyToken(token)
 	require.Error(t, err)
 	require.EqualError(t, err, ErrExpiredToken.Error())
 	require.Nil(t, payload)
--- token/maker.go
+++ token/maker.go
@@ -6,7 +6,7 @@ import "time"
 type Maker interface {
 
 	// CreateToken creates a new token for a specific username and duration
-	CreateToken(username string, duration time.Duration) (string, error)
+	CreateToken(username string, duration time.Duration) (string, *Payload, error)
 
 	// VerifyToken checks if the token is valid or not 
 	VerifyToken(token string) (*Payload, error)
